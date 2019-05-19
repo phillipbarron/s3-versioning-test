4e515583835ad042cf5f2ae036a83bff04471b2e
Phill Barron <phillipbarron242@gmail.com>
2019-05-19 11:19:33 +0100

    now storing each as a separate doc with common prefix

--- src/s3-service.js
+++ src/s3-service.js
@@ -1,53 +1,95 @@
-const cpsWormhole = require('@bbc/cps-wormhole');
-const uuidv1 = require('uuid/v1');
+const cpsWormhole = require("@bbc/cps-wormhole");
+const uuidv1 = require("uuid/v1");
 const s3ClientFactory = require("./aws-factory");
 
-
 const updateDocument = async (assetId, content) => {
-    const s3Client = s3ClientFactory.getS3Client();
-    const eventId = uuidv1();
-    const objectParameters = {
-        Body: JSON.stringify(content),
-        Key: `${assetId}.json`,
-        Bucket: 'cps-article-history-dev',
-        Metadata: {
-            eventId
-        }
+  const s3Client = s3ClientFactory.getS3Client();
+  const eventId = uuidv1();
+  const objectParameters = {
+    Body: JSON.stringify(content),
+    Key: `${assetId}.json`,
+    Bucket: "cps-article-history-dev",
+    Metadata: {
+      eventId
     }
-    await cpsWormhole.setCredentials();
-    
-    return new Promise((resolve, reject) => {
-        s3Client.putObject(objectParameters, function(err, data) {
-            if (err) {
-                reject(err)
-            }
+  };
+  await cpsWormhole.setCredentials();
 
-            resolve(data);
-          });
+  return new Promise((resolve, reject) => {
+    s3Client.putObject(objectParameters, function(err, data) {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(data);
     });
-}
+  });
+};
+
+const storeVersion = async (assetId, content) => {
+  const s3Client = s3ClientFactory.getS3Client();
+  const eventId = uuidv1();
+  const objectParameters = {
+    Body: JSON.stringify(content),
+    Key: `${assetId}/${eventId}.json`,
+    Bucket: "cps-article-history-dev"
+  };
+
+  await cpsWormhole.setCredentials();
+
+  return new Promise((resolve, reject) => {
+    s3Client.putObject(objectParameters, function(err, data) {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+};
 
 const getEventVersion = async (eventId, assetId) => {
-    const s3Client = s3ClientFactory.getS3Client();
+  const s3Client = s3ClientFactory.getS3Client();
 
-    const objectParameters = {
-        Prefix: `${assetId}.json`,
-        Bucket: 'cps-article-history-dev'
-    };
+  const objectParameters = {
+    Key: `${assetId}/{eventId}.json`,
+    Bucket: "cps-article-history-dev"
+  };
 
-    await cpsWormhole.setCredentials();
+  await cpsWormhole.setCredentials();
 
-    return new Promise((resolve, reject) => {
-        s3Client.listObjectVersions(objectParameters, function(err, data) {
-            if (err) {
-                reject(err)
-            }
-            resolve(data);
-        }); 
+  return new Promise((resolve, reject) => {
+    s3Client.getObject(objectParameters, function(err, data) {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
     });
-}
+  });
+};
+
+const listObjectVersions = async (eventId, assetId) => {
+  const s3Client = s3ClientFactory.getS3Client();
+
+  const objectParameters = {
+    Prefix: `${assetId}.json`,
+    Bucket: "cps-article-history-dev"
+  };
+
+  await cpsWormhole.setCredentials();
+
+  return new Promise((resolve, reject) => {
+    s3Client.listObjectVersions(objectParameters, function(err, data) {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+};
 
 module.exports = {
-    updateDocument,
-    getEventVersion
-}
+  updateDocument,
+  getEventVersion,
+  storeVersion,
+  listObjectVersions
+};
